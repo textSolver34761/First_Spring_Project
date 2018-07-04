3506cd08503f9b592597994b12aecd59645dd4db
textSolver34761 <pradonbenjamin@gmail.com>
2018-07-04 12:32:02 +0200

    Configuration

---
+++ projectbackend/src/main/java/net/bp/projectbackend/App.java
@@ -0,0 +1,13 @@
+package net.bp.projectbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
